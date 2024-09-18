df2d8728f2ee80bf077dc7240d4c40ceed248612
lennon-jesus <161793432+lennon-jesus@users.noreply.github.com>
2024-09-17 21:23:01 -0300

    Add files via upload

--- formulario.php
+++ formulario.php
@@ -10,7 +10,7 @@
         <label for="nome">Nome do aluno:      </label>
         <input name="nome" id="nome" type="text"><br>
         <label for="nasc">Data de nascimento: (DD/MM/AAAA)</label>
-        <input name="nasc" id="nasc" type="text"><br>
+        <input name="nasc" id="nasc" type="date"><br>
         <label for="cpf">CPF:                 </label>
         <input name="cpf" id="cpf" type="number"><br>
         <label for="matr">Matrícula:          </label>
